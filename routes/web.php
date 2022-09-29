dc242f729caed2cd122a2d2479a7f69476885cf5
DirectorCompany <58828108+directorcompany@users.noreply.github.com>
2022-09-29 22:38:48 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,3 @@ Auth::routes();
 Route::get('/user', [App\Http\Controllers\HomeController::class, 'user'])->name('user');
 Route::get('/username', [App\Http\Controllers\HomeController::class, 'username'])->name('username');
 Route::get('/order/{name}/{prod}', [App\Http\Controllers\HomeController::class, 'order'])->name('name');
-// Route::get('/coin', [App\Http\Controllers\HomeController::class, 'coin'])->name('coin');
-// Route::get('/product', [App\Http\Controllers\HomeController::class, 'product'])->name('product');
-// Route::get('/order', [App\Http\Controllers\HomeController::class, 'order'])->name('order');
